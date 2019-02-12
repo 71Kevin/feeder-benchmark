cfdc56329663ecdaf281a3326f05d8aedc9509f0
71Kevin <kevin.oliveira@pontaltelecom.com.br>
2019-02-12 16:35:05 -0200

    12/02/2019

--- app/util/contact-util.js
+++ app/util/contact-util.js
@@ -1,5 +1,6 @@
 const mongo = require('../mongoose/connect');
 const Contact = require('../mongoose/models/contact.js');
+const uuidv1 = require('uuid/v1');
 
 const contact = {
     async insertContacts(req, res) {
@@ -8,7 +9,31 @@ const contact = {
         for (let i = 0; i < quantity; i++) {
             let objContact = {
                 mailingId: mailingId,
-                contactId: i
+                contactId: uuidv1(),
+                sync: false,
+                clientId: 123,
+                clientName: "litle",
+                mailingName: "BENCHMARK",
+                campaignId: 66,
+                campaignName: "Lord of Cinder",
+                status: "New",
+                numbers: [
+                    1947
+                ],
+                vars: [],
+                cpf: 123,
+                reference: 123,
+                gathered: false,
+                createdAt: "",
+                numberType: [
+                    "Landline"
+                ],
+                currentNumber: 0,
+                counters: [
+                ],
+                renitency_at: false,
+                uraEvents: [],
+                updatedAt: false
             }
             contacts.push(objContact);
         }
