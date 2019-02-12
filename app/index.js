8a613994ba06c156f210b433032c4335bc40530f
71Kevin <kevin.oliveira@pontaltelecom.com.br>
2019-02-12 14:11:44 -0200

    12/02/2019

--- app/index.js
+++ app/index.js
@@ -2,21 +2,44 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongo = require('./mongoose/connect');
-const contactUtil = require('./util/contact-util');
+const Contact = require('./mongoose/models/contact.js');
+
 app.use(bodyParser.json()); 
+
+// (async function(){
+
+//   await mongo.connect();
+//   app.post("/test", function(request, response, next) {    
+//     const contact = new Contact({
+//         contact: request.body.contactId,
+//         mailingId: request.body.mailingId,
+
+//     });
+
+//     contact.save(function(err, u) {
+//         if (err) return next(err);
+//         return request.json(u);
+
+//     });
+//   });
+// })();
 
 
 (async function(){
   await mongo.connect();
   app.post('/insertContacts', function (req, res) {
+    const contact = new Contact({
+      quantity: req.body.quantity
+    });
     console.log(req.body);
-    res.send('ok')
+    res.send('ok');
+    
+    contact.save(function(err, u) {
+      if (err) return next(err);
+      return res.json(u);
+    });
   });
-
-  app.get('/test', function(req, res){
-
-  });
-
+  
   app.listen(process.env.PORT, '0.0.0.0');
-
+  
 })();
--- app/mongoose/models/contact.js
+++ app/mongoose/models/contact.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const contactSchema = new Schema({    
-    contactId: Number,
-    mailingId: Number
+    quantity: Number
 });
 
 module.exports = mongoose.model('contact', contactSchema);
